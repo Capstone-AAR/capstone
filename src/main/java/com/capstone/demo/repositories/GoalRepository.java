89aad506306163b9796354f517fbd7c725a090f0
andersoncardoso <anderson.d.cardoso@hotmail.com>
2017-11-21 11:11:26 -0600

    fix goal repository

--- src/main/java/com/capstone/demo/repositories/GoalRepository.java
+++ src/main/java/com/capstone/demo/repositories/GoalRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface GoalRepository extends CrudRepository<Goal,Long> {
-    Goal findByTitle(String title);
+    Goal findByGoalName(String goalName);
 
     @Query("from Goal g where g.id=?1")
     public Goal findById(long id);
