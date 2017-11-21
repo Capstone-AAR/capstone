9c0cf916ab7b7a6527a0998a1899257fb578d5b0
alxmpotter <alxmpotter@gmail.com>
2017-11-21 12:31:54 -0600

    child profile
    *add view for child profile
    *make sure view is loading with navbar

--- src/main/java/com/capstone/demo/controllers/ChildController.java
+++ src/main/java/com/capstone/demo/controllers/ChildController.java
@@ -1,14 +1,19 @@
 package com.capstone.demo.controllers;
 
+import com.capstone.demo.models.Child;
 import com.capstone.demo.repositories.ChildRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChildController {
-    private ChildRepository childDao;
+    @Autowired
+    ChildRepository childDao;
 
-    public ChildController(ChildRepository childDao) {
-        this.childDao = childDao;
+    @GetMapping("/child-profile")
+    public String childHomePage() {
+        return "users/child-profile";
     }
 
 
