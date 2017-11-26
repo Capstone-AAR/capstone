f3dd7aa86ba64250c5f74c21f17cb277d68cbe89
alxmpotter <alxmpotter@gmail.com>
2017-11-26 12:07:35 -0600

    child profile
    *add view for child profile
    *make sure view is loading with navbar

--- src/main/java/com/capstone/demo/controllers/TaskController.java
+++ src/main/java/com/capstone/demo/controllers/TaskController.java
@@ -2,6 +2,7 @@ package com.capstone.demo.controllers;
 
 import com.capstone.demo.models.Task;
 import com.capstone.demo.repositories.Tasks;
+import com.capstone.demo.services.TaskSvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,18 +12,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class TaskController {
-    @Autowired
-    Tasks taskDao;
+//    initializer when not using dummy data
+//    @Autowired
+//    Tasks taskDao;
+
+
+    /////using service for dummy data
+    private final TaskSvc taskSvc;
+    public TaskController(TaskSvc taskSvc) {
+        this.taskSvc = taskSvc;
+    }
 
     //////////
     //method uses tasks repo to find tasks and pass to view
     //////////
-    @GetMapping("/child-profile")
-    public String showAll(@PathVariable Model model){
-        Iterable<Task> tasks = taskDao.findAll();
-        model.addAttribute("task", tasks);
+    @GetMapping("/tasks")
+    public String showAll(Model model){
+        Iterable<Task> tasks = taskSvc.findAll();
+        model.addAttribute("tasks", tasks);
 
-        return "users/child-profile";
+        return "users/tasks";
     }
 
 }
