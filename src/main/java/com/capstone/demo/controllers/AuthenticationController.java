2331030b89f44f9f76c5c69603ba98c3061f8b10
rosaliework <rosalie.gomez12@gmail.com>
2017-11-28 09:44:55 -0600

    child-login

--- src/main/java/com/capstone/demo/controllers/AuthenticationController.java
+++ src/main/java/com/capstone/demo/controllers/AuthenticationController.java
@@ -2,11 +2,19 @@ package com.capstone.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthenticationController {
     @GetMapping("/login")
     public String showLoginForm() {
         return "users/login";
+    }
+
+
+
+    @GetMapping("/child-login")
+    public String childLogin(){
+        return "users/child-login";
     }
 }
