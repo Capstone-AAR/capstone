198f6026fb4babba4e5475b1f54a7277c6eb81bb
andersoncardoso <anderson.d.cardoso@hotmail.com>
2017-11-28 16:53:46 -0600

    Merge branch 'master' of github.com:Capstone-AAR/capstone into task-util
    # Conflicts:
    #	src/main/java/com/capstone/demo/controllers/TaskController.java
    #	src/main/java/com/capstone/demo/models/Task.java
    #	src/main/java/com/capstone/demo/repositories/TaskRepository.java
    #	src/main/resources/templates/goals/create.html
    #	src/main/resources/templates/users/parent-profile.html
    #	src/main/resources/templates/users/tasks.html

--- src/main/java/com/capstone/demo/repositories/TaskRepository.java
+++ src/main/java/com/capstone/demo/repositories/TaskRepository.java
@@ -9,7 +9,5 @@ import java.util.List;
 
 @Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
-
-public interface Tasks extends CrudRepository<Task, Long> {
-    List<Task> findByStatus(TaskStatus status);
+//    List<Task> findByStatus(TaskStatus status);
 }
