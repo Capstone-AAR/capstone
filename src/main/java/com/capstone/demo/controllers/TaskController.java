7c38cd2aa8c454b8d980bf435e82ce1b30b133e2
alxmpotter <alxmpotter@gmail.com>
2017-11-28 12:51:48 -0600

    status
    *delete status table
    *use enum for task status state
    *work on implementing methods in task controller to show task status

--- src/main/java/com/capstone/demo/controllers/TaskController.java
+++ src/main/java/com/capstone/demo/controllers/TaskController.java
@@ -1,13 +1,14 @@
 package com.capstone.demo.controllers;
 
 import com.capstone.demo.models.Task;
+import com.capstone.demo.models.TaskStatus;
 import com.capstone.demo.repositories.Tasks;
 import com.capstone.demo.services.TaskSvc;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 
 @Controller
@@ -16,11 +17,13 @@ public class TaskController {
 //    @Autowired
 //    Tasks taskDao;
 
-
     /////using service for dummy data
     private final TaskSvc taskSvc;
-    public TaskController(TaskSvc taskSvc) {
+    private Tasks tasks;
+
+    public TaskController(TaskSvc taskSvc, Tasks tasks) {
         this.taskSvc = taskSvc;
+        this.tasks = tasks;
     }
 
     //////////
@@ -33,5 +36,21 @@ public class TaskController {
 
         return "users/tasks";
     }
+
+
+
+////Trying to work with Java class Enum to show task status
+
+//    @GetMapping("/tasks")
+//    public String showAll(Model model){
+//        Iterable<Task> completedTasks = tasks.findByStatus(TaskStatus.APPROVED); //querying for task status that are approved
+//
+//        Task task = new Task();
+//        task.getStatus().equals(TaskStatus.APPROVED);
+//        model.addAttribute("tasks", completedTasks);
+//
+//        return "users/tasks";
+//    }
+
 
 }
