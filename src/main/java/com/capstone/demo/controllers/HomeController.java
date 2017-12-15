fcb5a4ace792a5033c877f2eaa27ebd2eba5cf2f
andersoncardoso <anderson.d.cardoso@hotmail.com>
2017-12-15 17:29:45 -0600

    adding comments to pages.

--- src/main/java/com/capstone/demo/controllers/HomeController.java
+++ src/main/java/com/capstone/demo/controllers/HomeController.java
@@ -1,8 +1,14 @@
 package com.capstone.demo.controllers;
 
+/////////////////////////////////////////////////////
+// Libraries imported and being used in this class.
+/////////////////////////////////////////////////////
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+///////////////////////////////////////////////////////////////
+// Take user to mock landing page. Page contains simple text.
+///////////////////////////////////////////////////////////////
 public class HomeController {
     @GetMapping("/")
     @ResponseBody
--- src/main/java/com/capstone/demo/controllers/ParentsController.java
+++ src/main/java/com/capstone/demo/controllers/ParentsController.java
@@ -1,5 +1,9 @@
 package com.capstone.demo.controllers;
 
+/////////////////////////////////////////////////////
+// Libraries imported and being used in this class.
+/////////////////////////////////////////////////////
+
 import com.capstone.demo.models.Parent;
 import com.capstone.demo.models.User;
 import com.capstone.demo.repositories.ChildRepository;
@@ -16,14 +20,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 
+
 @Controller
 public class ParentsController {
+
+    //////// ATTRIBUTES ////////////
+    ////////////////////////////////
+    // Private fields(attributes)
+    ////////////////////////////////
     private UserRepository repository;
     private PasswordEncoder encoder;
     private final ParentRepository parentRepository;
     private final ChildRepository childRepository;
     private final UserService userService;
 
+
+    ///////////////////// CONSTRUCTOR METHOD /////////////////////////////
+    //////////////////////////////////////////////////////////////////////
+    // Constructor method used to initialize this class when instantiated.
+    // Also, dependency injection is being used in order to facilitate the
+    // usage of methods and attributes that belong to other classes.
+    //////////////////////////////////////////////////////////////////////
     public ParentsController(
             PasswordEncoder encoder,
             UserRepository repository,
@@ -38,11 +55,17 @@ public class ParentsController {
         this.userService = userService;
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////// METHODS ///////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     //////////////////////////////////////////////////////////
-    // Home page.ds
+    // Display index page to user.
     //////////////////////////////////////////////////////////
     @GetMapping("/")
     public String homePage() {
+
+        ////////////////////////////
         return "users/index";
     }
 
@@ -51,30 +74,46 @@ public class ParentsController {
     ////////////////////////////////////////////////////////
     @GetMapping("/register")
     public String registerPage(Model viewModel) {
+
+        //////////////////////////////
         viewModel.addAttribute("parent", new User());
+
+        /////////////////////////////
         return "users/register";
     }
 
+    ////////////////////////////////////////////////////////
+    // Persist new user data to user table.
+    ////////////////////////////////////////////////////////
     @PostMapping("/users/register")
     public String registerForm(@Valid User user, Errors validation, Model model) {
 
+        /////////////////////////////////////
         String username = user.getUsername();
+
+        //////////////////////////////////
         User existingUsername = repository.findByUsername(username);
+
+        /////////////////////////////////
         User existingEmail = repository.findByEmail(user.getEmail());
 
-        if(existingUsername != null) {
+        ////////////////////////////////////////////////////////
+        // Validating new user.
+        ////////////////////////////////////////////////////////
+        if (existingUsername != null) {
             validation.rejectValue("username", "user.username", "Duplicated username " + username);
         }
 
-        if(existingEmail != null) {
+        if (existingEmail != null) {
             validation.rejectValue("email", "user.email", "Duplicated email " + user.getEmail());
         }
 
-        if(validation.hasErrors()) {
+        if (validation.hasErrors()) {
             model.addAttribute("errors", validation);
             model.addAttribute("parent", user);
             return "users/register";
         }
+
 
         user.setPassword(encoder.encode(user.getPassword()));
         user.isAParent();
@@ -84,6 +123,9 @@ public class ParentsController {
         parent.setUser(user);
         parentRepository.save(parent);
 
+        /////////////////////////////////////////////////////////////////////
+        // Redirect user to login page in order to login with new account.
+        /////////////////////////////////////////////////////////////////////
         return "redirect:/login";
     }
 
