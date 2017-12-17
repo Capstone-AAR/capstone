fe80cda625c5cd642cac81582e7b1fc559e41a15
andersoncardoso <anderson.d.cardoso@hotmail.com>
2017-12-16 23:23:49 -0600

    adding comments to pages.

--- src/main/java/com/capstone/demo/repositories/ChildRepository.java
+++ src/main/java/com/capstone/demo/repositories/ChildRepository.java
@@ -1,5 +1,8 @@
 package com.capstone.demo.repositories;
 
+/////////////////////////////////////////////////////
+// Libraries imported and being used in this class.
+/////////////////////////////////////////////////////
 import com.capstone.demo.models.Child;
 import com.capstone.demo.models.User;
 import org.springframework.data.jpa.repository.Query;
@@ -7,12 +10,17 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
+/////////////////////////////////////////////////////////////////
+// Repository is an interface used create simple queries used to
+// create, reade, update, delete data in my database.
+/////////////////////////////////////////////////////////////////
 public interface ChildRepository extends CrudRepository<Child, Long> {
+
     Child findByUser(User user);
 
     Child findByUserId(Long id);
 
-    @Query(nativeQuery = true, value="SELECT * from children where parent_id=?1")
+    @Query(nativeQuery = true, value = "SELECT * from children where parent_id=?1")
     List<Child> findAllByParentId(Long id);
 
 
--- src/main/java/com/capstone/demo/repositories/TaskRepository.java
+++ src/main/java/com/capstone/demo/repositories/TaskRepository.java
@@ -1,5 +1,8 @@
 package com.capstone.demo.repositories;
 
+/////////////////////////////////////////////////////
+// Libraries imported and being used in this class.
+/////////////////////////////////////////////////////
 import com.capstone.demo.models.Task;
 import com.capstone.demo.models.TaskStatus;
 import org.springframework.data.repository.CrudRepository;
@@ -7,8 +10,13 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/////////////////////////////////////////////////////////////////
+// Repository is an interface used create simple queries used to
+// create, reade, update, delete data in my database.
+/////////////////////////////////////////////////////////////////
 @Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
+
     List<Task> findByStatus(TaskStatus status);
 
     List<Task> findByGoalId(Long goalId);
